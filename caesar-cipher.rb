4cd9d9149134f530e13063300b6382642a46a791
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-09-19 11:54:49 +0700

    Modify, simplify and refactoring caesar-cipher.rb

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,21 +1,9 @@
 def caesar_cipher(string, shift_key)
   string.each_char { |char|
     char_ascii = char.ord
-    if char_ascii.between?(65, 90)
-      cip_ascii = char_ascii + shift_key
-      if cip_ascii > 90
-        cip_ascii = cip_ascii - 26
-      elsif cip_ascii < 65
-        cip_ascii = cip_ascii + 26
-      end
-      print cip_ascii.chr
-    elsif char_ascii.between?(97, 122)
-      cip_ascii = char_ascii + shift_key
-      if cip_ascii > 122
-        cip_ascii = cip_ascii - 26
-      elsif cip_ascii < 97
-        cip_ascii = cip_ascii + 26
-      end
+    factor = char_ascii <= 90 ? 65 : 97    
+    if char_ascii.between?(65, 90) || char_ascii.between?(97, 122)
+      cip_ascii = (((char_ascii - factor) + shift_key) % 26) + factor
       print cip_ascii.chr
     else
       print char
@@ -24,4 +12,4 @@ def caesar_cipher(string, shift_key)
   puts "\n"
 end
 
-caesar_cipher("Ayo serang markas Jerman", -8)
+caesar_cipher("Ayo serang markas Jerman!!!", 8)
